b14cade195c951b41e57bc83869423db6cdee7f8
leegrindon <grindon@live.co.uk>
2016-06-20 16:56:23 +0100

    Added Exception Action enum and sql table with post script

---
+++ src/1. Layers/1.3 Domain/Well.Domain/Enums/ExceptionAction.cs
@@ -0,0 +1,17 @@
+﻿namespace PH.Well.Domain.Enums
+{
+    public enum ExceptionAction
+    {
+        Credit = 1,
+
+        CreditAndReorder = 2,
+
+        ReplanInRoadnet = 3,
+
+        ReplanInTranscend = 4,
+
+        ReplanInTheQueue = 5,
+
+        Reject = 6
+    }
+}

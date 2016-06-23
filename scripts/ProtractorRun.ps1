7520eaeff33a4f95f7afbc41cd6c1beff43fa2d4
Mike Hook <michael.hook@palmerharvey.co.uk>
2016-06-23 16:52:31 +0100

    Made test folder path configurable

--- scripts/ProtractorRun.ps1
+++ scripts/ProtractorRun.ps1
@@ -1,12 +1,14 @@
-﻿param($siteUrl)
+﻿param($uiTestFolder, $siteUrl)
 
+#$uiTestFolder = "..\src\4. Test\Well.Test.UI\"
 #$siteUrl = "http://ho-ms-qa1/well/dashboard/"
 
 cls
 function RunProtractorTests
 {
-	$fileExe = "..\src\4. Test\Well.Test.UI\node_modules\.bin\protractor"
-	$result = & $fileExe "..\src\4. Test\Well.Test.UI\conf.js" --params.baseUrl=$siteUrl
+	$fileExe = $uiTestFolder + "node_modules\.bin\protractor"
+    $confPath = $uiTestFolder + "conf.js"
+	$result = & $fileExe $confPath --params.baseUrl=$siteUrl
 	$code = $LASTEXITCODE
 	$exitC = 1
 	if ($code -eq 0 -or $code -eq 110 -or $code -eq 120 -or $code -eq 210 -or $code -eq 430)

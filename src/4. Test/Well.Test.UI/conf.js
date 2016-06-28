80853f3faa93df936b573f99e045a7e4e5404cf3
Mike Hook <michael.hook@palmerharvey.co.uk>
2016-06-28 10:28:23 +0100

    Removed orig merge files. Added js files to project.

--- src/4. Test/Well.Test.UI/conf.js
+++ src/4. Test/Well.Test.UI/conf.js
@@ -3,7 +3,6 @@ exports.config = {
     framework: 'jasmine2',
     directConnect: true,
     useAllAngular2AppRoots: true,
-    rootElement: "[ow-app]",
     //seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['Specs/*.js'],
     onPrepare: function () {

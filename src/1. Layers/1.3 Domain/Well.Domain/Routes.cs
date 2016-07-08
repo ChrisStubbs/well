c271a3972895e1f7def6d2c361ad1895f2e2c678
gary.williams <Gary.Williams@palmerharvey.co.uk>
2016-07-08 10:02:24 +0100

    commited sql project/domain

---
+++ src/1. Layers/1.3 Domain/Well.Domain/Routes.cs
@@ -0,0 +1,24 @@
+﻿namespace PH.Well.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Xml.Serialization;
+    [Serializable()]
+    //[XmlRoot("Routes")]
+    public class Routes: Entity<int>
+    { 
+        public Routes()
+        {
+            this.RouteHeaders = new Collection<RouteHeader>();
+        }
+
+        public string FileName { get; set; }
+
+        public DateTime ImportDate { get; set; }
+
+        //[XmlArray("RouteHeaders")]
+        //[XmlArrayItem("RouteHeader", typeof(RouteHeader))]
+        public Collection<RouteHeader> RouteHeaders { get; set; }
+    }
+}
--- src/1. Layers/1.4 Infrastructure/Well.Repositories/StoredProcedures.cs
+++ src/1. Layers/1.4 Infrastructure/Well.Repositories/StoredProcedures.cs
@@ -8,6 +8,10 @@
         //route header
         public static string RouteHeaderGetCleanDeliveries = "RouteHeader_GetCleanDeliveries";       
         public static string RouteHeadersGet = "RouteHeaders_Get";
+        public static string RoutesCheckDuplicate = "Routes_CheckDuplicate";
+        public static string RoutesCreateOrUpdate = "Routes_CreateOrUpdate";
+        public static string RoutesGetById = "Routes_GetById";
+        
 
         //Stops
         public static string StopsGetByRouteHeaderId = "Stops_GetByRouteHeaderId";

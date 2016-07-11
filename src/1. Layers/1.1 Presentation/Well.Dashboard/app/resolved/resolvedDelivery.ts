77fe80f54341d0d7b82c8cc2b78c9ee13c680e4d
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2016-07-11 15:40:33 +0100

    Re- add resolvedDelivery

---
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/resolved/resolvedDelivery.ts
@@ -0,0 +1,11 @@
+﻿export interface IResolvedDelivery {
+    route: string;
+    drop: string;
+    invoiceNo: number;
+    account: string;
+    accountName: string;
+    status: string;
+    action: string;
+    assigned: string;
+    dateTime: string;
+}

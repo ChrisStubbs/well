357fb2c8f79c041307c21a791f3970fb568c8478
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2016-07-12 15:43:21 +0100

    Filter Option component

---
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/dropDownItem.ts
@@ -0,0 +1,9 @@
+﻿export class DropDownItem {
+    description: string;
+    value: string;
+
+    constructor(description: string = "", value: string = "") {
+        this.description = description;
+        this.value = value;
+    }
+}
---
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/optionfilter.component.ts
@@ -0,0 +1,35 @@
+﻿import {Component, OnChanges, Input, Output, EventEmitter} from "angular2/core";
+import {FilterOption} from "./filterOption";
+import {DropDownItem} from "./dropDownItem";
+
+@Component({
+    selector: "ow-optionfilter",
+    templateUrl: "app/shared/optionfilter.component.html"
+})
+export class OptionFilterComponent implements OnChanges {
+
+    private defaultOption: DropDownItem = new DropDownItem("Option", "");
+
+    @Input() options: DropDownItem[];
+    filterText: string;
+    selectedOption: DropDownItem = this.defaultOption;
+    @Output() filterClicked: EventEmitter<FilterOption> = new EventEmitter<FilterOption>();
+
+    ngOnChanges(): void {
+        console.log("onchange");
+    }
+
+    clearFilterText(): void {
+        this.filterText = '';
+        this.selectedOption = this.defaultOption;
+        this.applyFilter();
+    }
+
+    applyFilter(): void {
+        this.filterClicked.emit(new FilterOption(this.selectedOption, this.filterText));
+    }
+
+    setSelectedOption(option: DropDownItem): void {
+        this.selectedOption = option;
+    }
+}

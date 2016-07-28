8458e4b9d0a361c5b53bd84200e45bb3ac1188c8
leegrindon <grindon@live.co.uk>
2016-07-28 16:26:34 +0100

    Fixed stored procs

--- src/1. Layers/1.1 Presentation/Well.Api/Controllers/DeliveryController.cs
+++ src/1. Layers/1.1 Presentation/Well.Api/Controllers/DeliveryController.cs
@@ -1,18 +1,13 @@
-﻿using System.Threading;
-
-namespace PH.Well.Api.Controllers
+﻿namespace PH.Well.Api.Controllers
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Web.Http;
     using Common.Contracts;
-    using Common.Extensions;
     using Models;
     using Repositories.Contracts;
-    using Well.Domain.ValueObjects;
 
     public class DeliveryController : BaseApiController
     {
@@ -30,7 +25,6 @@ namespace PH.Well.Api.Controllers
             this.deliveryReadRepository = deliveryReadRepository;
             this.serverErrorResponseHandler = serverErrorResponseHandler;
         }
-
 
         [HttpGet]
         [Route("deliveries/exception", Name = "GetExceptions")]
@@ -57,7 +51,6 @@ namespace PH.Well.Api.Controllers
         {
             try
             {
-                logger.LogDebug(Thread.CurrentPrincipal.Identity.Name);
                 var cleanDeliveries = this.deliveryReadRepository.GetCleanDeliveries(this.UserName).ToList();
 
                 return !cleanDeliveries.Any()
@@ -70,8 +63,6 @@ namespace PH.Well.Api.Controllers
                 return this.serverErrorResponseHandler.HandleException(Request, ex);
             }
         }
-
-
 
         [HttpGet]
         [Route("deliveries/resolved", Name = "GetResolvedDeliveries")]
@@ -97,11 +88,7 @@ namespace PH.Well.Api.Controllers
         {
             try
             {
-                //Todo: Replace this with call to database and mapping!
-                var delivery = this.GetMockDeliveryDetails(id);
-                return (delivery == null)
-                   ? this.Request.CreateResponse(HttpStatusCode.NotFound)
-                   : this.Request.CreateResponse(HttpStatusCode.OK, delivery);
+                return this.Request.CreateResponse(HttpStatusCode.OK);
             }
             catch (Exception ex)
             {
@@ -109,85 +96,5 @@ namespace PH.Well.Api.Controllers
                 return serverErrorResponseHandler.HandleException(Request, ex);
             }
         }
-
-
-        private DeliveryDetailModel GetMockDeliveryDetails(int id)
-        {
-            var deliveryDetail = new DeliveryDetailModel
-            {
-                Id = id,
-                AccountCode = "4895.002",
-                AccountName = "Fags bags and Mags",
-                AccountAddress = "Gaddesdon Place, Great Gaddesdon, Hemel Hempstead HP2 6EX",
-                InvoiceNumber = "1363291",
-                ContactName = "Chuck Chidlow",
-                PhoneNumber = "01234 678901",
-                MobileNumber = "07756 914463",
-                DeliveryType = "Exception"
-            };
-
-            for (int i = 0; i < 5; i++)
-            {
-                deliveryDetail.DeliveryLines.Add(new DeliveryLineModel
-                {
-                    LineNo = i,
-                    ProductCode = "19152",
-                    ProductDescription = "Blu Cart Ref C/Tob12mg3s",
-                    Value = "149.70",
-                    InvoicedQuantity = 10,
-                    DeliveredQuantity = 7,
-                    DamagedQuantity = 0,
-                    ShortQuantity = 3,
-                    Reason = "Picking Error",
-                    Status = "Short",
-                    Action = "Credited"
-                });
-                deliveryDetail.DeliveryLines.Add(new DeliveryLineModel
-                {
-                    LineNo = i + 1,
-                    ProductCode = "19144",
-                    ProductDescription = "Blu Cart Ref Gold18mg 3s",
-                    Value = "29.94",
-                    InvoicedQuantity = 2,
-                    DeliveredQuantity = 2,
-                    DamagedQuantity = 1,
-                    ShortQuantity = 0,
-                    Status = "Damaged",
-                    Action = "Credited"
-                });
-                deliveryDetail.DeliveryLines.Add(new DeliveryLineModel
-                {
-                    LineNo = i + 2,
-                    ProductCode = "19136",
-                    ProductDescription = "Blu Clearomiser",
-                    Value = "299.90",
-                    InvoicedQuantity = 19,
-                    DeliveredQuantity = 8,
-                    DamagedQuantity = 0,
-                    ShortQuantity = 11,
-                    Reason = "Short deliverd",
-                    Status = "Short",
-                    Action = "Credited"
-                });
-                deliveryDetail.DeliveryLines.Add(new DeliveryLineModel
-                {
-                    LineNo = i + 2,
-                    ProductCode = "19500",
-                    ProductDescription = "Marlboro Reds 20s",
-                    Value = "299.90",
-                    InvoicedQuantity = 20,
-                    DeliveredQuantity = 20,
-                    DamagedQuantity = 0,
-                    ShortQuantity = 0,
-                    Reason = "Short deliverd",
-                    Status = "Complete"
-                });
-            }
-            return deliveryDetail;
-        }
-
-
-
-
     }
 }

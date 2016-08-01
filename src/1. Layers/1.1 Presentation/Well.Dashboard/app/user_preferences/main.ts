af62ecce1e75bfddb90391697b2de61fa29cd0a9
leegrindon <grindon@live.co.uk>
2016-08-01 09:58:51 +0100

    Force team city

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/user_preferences/main.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/user_preferences/main.ts
@@ -1,1 +1,9 @@
-﻿
+﻿import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { provide } from "@angular/core";
+import { IGlobalSettings } from '../shared/globalSettings';
+
+import {UserPreferenceComponent} from './userPreferenceComponent';
+
+export function runApplication(config: IGlobalSettings) {
+    bootstrap(UserPreferenceComponent, [provide('global.settings', { useValue: config })]);
+}

86dafd6968ad0dccb76648bb0962bfcb61cc61a8
Mike Hook <michael.hook@palmerharvey.co.uk>
2016-08-03 10:24:40 +0100

    Removed redundant main files.

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/exceptions/main.ts
+++
@@ -1,9 +0,0 @@
-﻿import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { provide } from "@angular/core";
-import { IGlobalSettings } from '../shared/globalSettings';
-
-import {ExceptionsComponent} from './exceptionsComponent';
-
-export function runApplication(config: IGlobalSettings) {
-    bootstrap(ExceptionsComponent, [provide('global.settings', { useValue: config })]);
-}

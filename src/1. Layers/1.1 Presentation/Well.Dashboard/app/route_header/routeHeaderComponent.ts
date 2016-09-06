631949668e212baf97eede5497a41bf9beefbd5d
leegrindon <grindon@live.co.uk>
2016-09-06 12:22:43 +0100

    Context menu moving to modal

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/route_header/routeHeaderComponent.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/route_header/routeHeaderComponent.ts
@@ -4,11 +4,12 @@ import 'rxjs/Rx';   // Load all features
 import {PaginationService } from 'ng2-pagination';
 import {IRoute} from './route';
 import {RouteHeaderService} from './routeHeaderService';
-import {DropDownItem} from "../shared/dropDownItem";
-import Option = require("../shared/filterOption");
+import {DropDownItem} from '../shared/dropDownItem';
+import Option = require('../shared/filterOption');
 import FilterOption = Option.FilterOption;
-import {WellModal} from "../shared/well-modal";
-import {RefreshService} from "../shared/refreshService";
+import {WellModal} from '../shared/well-modal';
+import {RefreshService} from '../shared/refreshService';
+import {ContextMenuItem} from './contextMenuItem';
 
 @Component({
     selector: 'ow-routes',
@@ -21,7 +22,7 @@ export class RouteHeaderComponent implements OnInit {
     routes: IRoute[];
     rowCount: number = 10;
     currentConfigSort: string;
-
+    isContextMenuVisible = false;
     lastRefresh = Date.now();
     filterOption: Option.FilterOption = new FilterOption();
     options: DropDownItem[] = [
@@ -30,10 +31,13 @@ export class RouteHeaderComponent implements OnInit {
         new DropDownItem("Invoice", "invoice", true),
         new DropDownItem("Assignee", "assignee", true)
     ];
+    items: ContextMenuItem[];
+
+    private mouseLocation: { left: number, top: number } = { left: 0, top: 0 };
 
     constructor(
         private routerHeaderService: RouteHeaderService,
-        private refreshService: RefreshService){
+        private refreshService: RefreshService) {
     }
 
     @ViewChild(WellModal) modal = new WellModal();
@@ -42,6 +46,7 @@ export class RouteHeaderComponent implements OnInit {
         this.refreshSubscription = this.refreshService.dataRefreshed$.subscribe(r => this.getRoutes());
         this.getRoutes();
         this.currentConfigSort = '-dateTimeUpdated';
+        this.items = [{ name: 'View exceptions' },{ name: 'View clean' }];
     }
 
     ngOnDestroy() {
@@ -62,7 +67,8 @@ export class RouteHeaderComponent implements OnInit {
                 error => this.lastRefresh = Date.now());
     }
 
-    routeSelected(route): void {
+    routeSelected($event: MouseEvent, route): void {
+        this.showMenu($event);
     }
 
     onFilterClicked(filterOption: FilterOption) {
@@ -73,6 +79,28 @@ export class RouteHeaderComponent implements OnInit {
         } else {
             this.filterOption = filterOption;
         }
+    }
+
+    get locationCss() {
+        return {
+            position: 'fixed',
+            border: '1px solid #ddd',
+            display: this.isContextMenuVisible ? 'block' : 'none',
+            left: this.mouseLocation.left + 'px',
+            top: this.mouseLocation.top + 'px'
+        };
+    }
+
+    showMenu(event) {
+        this.isContextMenuVisible = true;
+        this.mouseLocation = {
+            left: event.clientX,
+            top: event.clientY
+        }
+    }
+
+    closeMenu() {
+        this.isContextMenuVisible = false;
     }
 }
 

2e8c968e93508b08b8e720b059f62583d6ad2f8d
leegrindon <grindon@live.co.uk>
2016-09-08 17:07:02 +0100

    Feature file class refresh

--- src/4. Test/BDD/Steps/Page/ExceptionPageSteps.cs
+++ src/4. Test/BDD/Steps/Page/ExceptionPageSteps.cs
@@ -62,7 +62,7 @@
             Assert.That(ExceptionDeliveriesPage.Pager.NoOfPages(), Is.EqualTo(noOfPages));
         }
 
-        [When(@"I click on exception row")]
+        [When(@"I click on exception row 4")]
         public void ClickExceptionDetail()
         {
             var rows = this.ExceptionDeliveriesPage.ExceptionsGrid.ReturnAllRows().ToList();

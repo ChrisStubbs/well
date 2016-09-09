37196ecdd5ac09e1b3391919b78be1f4fe974c24
Michael Hook <michael.hook@palmerharvey.co.uk>
2016-09-09 11:12:14 +0100

    EOW-216 Updated widgets UI to place bar chart on right hand side.

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/home/widgetGraphComponent.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/home/widgetGraphComponent.ts
@@ -28,63 +28,27 @@ export class WidgetGraphComponent implements OnInit {
 
     }
 
-    // lineChart
-    public lineChartData: Array<any> = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Exceptions' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Assigned' },
-        { data: [18, 48, 77, 9, 100, 27, 40], label: 'Outstanding' }
-    ];
-    public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    public lineChartOptions: any = {
-        animation: false,
+    public barChartOptions: any = {
+        scaleShowVerticalLines: false,
         responsive: true
     };
-    public lineChartColors: Array<any> = [
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        },
-        { // dark grey
-            backgroundColor: 'rgba(77,83,96,0.2)',
-            borderColor: 'rgba(77,83,96,1)',
-            pointBackgroundColor: 'rgba(77,83,96,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(77,83,96,1)'
-        },
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    public barChartLabels: string[] = ['Exceptions', 'Assigned', 'Outstanding', 'Notifications'];
+    public barChartType: string = 'bar';
+    public barChartLegend: boolean = false;
+    red: string = "#f2dede";
+    redLine: string = "#a94442";
+    blue: string = "#428bca";
+
+
+    public barChartColors: Array<any> = [
+        { // Exceptions
+            backgroundColor: [this.blue, this.blue, this.redLine, this.blue],
         }
     ];
-    public lineChartLegend: boolean = true;
-    public lineChartType: string = 'line';
 
-    public randomize(): void {
-        let _lineChartData: Array<any> = new Array(this.lineChartData.length);
-        for (let i = 0; i < this.lineChartData.length; i++) {
-            _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
-            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-                _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-            }
+    public barChartData: any[] = [
+        {
+            data: [5, 3, 10, 0]
         }
-        this.lineChartData = _lineChartData;
-    }
-
-    // events
-    public chartClicked(e: any): void {
-        console.log(e);
-    }
-
-    public chartHovered(e: any): void {
-        console.log(e);
-    }
+    ];
 }

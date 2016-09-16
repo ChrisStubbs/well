ecbbf42d0f5943f4237e8ee9a6a0b6a5257cbd56
leegrindon <grindon@live.co.uk>
2016-09-16 11:55:08 +0100

    Seasonal Date Controller

---
+++ src/1. Layers/1.1 Presentation/Well.Api/Controllers/SeasonalDateController.cs
@@ -0,0 +1,27 @@
+﻿namespace PH.Well.Api.Controllers
+{
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using PH.Well.Repositories.Contracts;
+
+    public class SeasonalDateController : BaseApiController
+    {
+        private readonly ISeasonalDateRepository seasonalDateRepository;
+
+        public SeasonalDateController(ISeasonalDateRepository seasonalDateRepository)
+        {
+            this.seasonalDateRepository = seasonalDateRepository;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var seasonalDates = this.seasonalDateRepository.GetAll().OrderBy(x => x.From).ToList();
+
+            return this.Request.CreateResponse(HttpStatusCode.OK, seasonalDates);
+        }
+    }
+}

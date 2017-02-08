02e9760202d281c0aa60e3d5ff72fc2210863f83
Michael Hook <michael.hook@palmerharvey.co.uk>
2017-02-08 14:59:23 +0000

    Fixed date format on widgets in IE

---
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/customDatePipe.ts
@@ -0,0 +1,12 @@
+﻿import {Pipe, PipeTransform} from '@angular/core';
+import * as moment from 'moment';
+
+@Pipe({ name: 'customDate' })
+export class CustomDatePipe implements PipeTransform {
+
+    transform(value: Date, format: string = ""): string {
+
+        if (!value) return "";
+        return moment(value).format(format);
+    }
+}

8bd5070bb883800636328ca70bf1fb388d0b551e
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-03-27 09:06:41 +0100

    Bulk Credit Bdd

---
+++ src/4. Test/BDD/Features/Pages/BulkCredit.feature
@@ -0,0 +1,15 @@
+﻿Feature: Bulk Credit Feature
+	As a user I need to be able to bulk credit Delievry Exceptions
+
+Background: 
+	Given I have a clean database
+	And I import the route file 'ROUTE_PLYM_BulkCredit.xml' into the well
+	And I have loaded the order file 'ORDER_PLY_BulkCredit.xml' into the well
+	And I have imported the following valid Epod files
+	| Filename              |
+	| ePOD__BulkCredit4.xml |
+	| ePOD__BulkCredit3.xml |
+	| ePOD__BulkCredit2.xml |
+	| ePOD__BulkCredit1.xml |
+
+	Scenario: A user with sufficient credit threshold set can bulk credit
--- src/4. Test/BDD/Steps/AdamImportSteps.cs
+++ src/4. Test/BDD/Steps/AdamImportSteps.cs
@@ -118,6 +118,16 @@
             this.adamFileMonitorService.Process(importFilePath);
         }
 
+        [Given(@"I have loaded the order file '(.*)' into the well")]
+        public void GivenIHaveLoadedTheOrderFileIntoTheWell(string fileName)
+        {
+            var importFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+               $"xml\\{fileName}"));
+
+            this.adamFileMonitorService.Process(importFilePath);
+        }
+
+
         [Given(@"I have loaded the Adam route data that has 21 lines")]
         public void GivenIHaveLoadedTheAdamRouteDataThatHasLines()
         {
@@ -145,7 +155,7 @@
         [When(@"I import the route file '(.*)' into the well")]
         public void WhenIImportTheRouteFileIntoTheWell(string routeFile)
         {
-            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RouteFiles");
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Xml");
 
             adamFileMonitorService.Process(Path.Combine(filePath, routeFile));
         }
@@ -205,6 +215,16 @@
                 this.epodUpdateService.Update(routes);
             }
         }
+
+        [Given(@"I have imported the following valid Epod files")]
+        public void GivenIHaveImportedTheFollowingValidEpodFiles(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                GivenIHaveImportedAValidEpodUpdateFile(row["Filename"]);
+            }
+        }
+
 
         [Given(@"I have an exception royalty of (.*) days for royalty (.*)")]
         public void GivenIHaveAnExceptionRoyaltyOfDaysForRoyalty(int exceptionDays, int royaltyCode)

8bdd6ea1c79818c394d61b6b7130ab085479f993
Henrry.Pires <henrry.pires@palmerharvey.co.uk>
2017-04-10 10:31:56 +0100

    making teamcity to set the current version

--- src/1. Layers/1.1 Presentation/Well.Dashboard/Global.asax.cs
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/Global.asax.cs
@@ -1,4 +1,9 @@
-﻿namespace PH.Well.Dashboard
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("de042783-a686-46e1-9a5f-1055f221ba14")]
+
+namespace PH.Well.Dashboard
 {
     using System.Web.Http;
     using System.Web.Mvc;
--- src/1. Layers/1.1 Presentation/Well.Dashboard/Properties/AssemblyInfo.cs
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/Properties/AssemblyInfo.cs
@@ -9,7 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("de042783-a686-46e1-9a5f-1055f221ba14")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

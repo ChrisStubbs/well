649a08db46ff3522dcef48067e3da54db9f7a433
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-05-03 14:11:30 +0100

    Fix filtering on route

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/branch/branch.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/branch/branch.ts
@@ -3,17 +3,4 @@
     public id: number;
     public name: string;
     public selected: boolean;
-
-    //constructor(obj: Branch = {} as Branch)
-    //{
-    //    const {
-    //        id = 0,
-    //        name = undefined,
-    //        selected = false
-    //     } = obj;
-
-    //    this.id = id;
-    //    this.name = name;
-    //    this.selected = selected;
-    //}
 }

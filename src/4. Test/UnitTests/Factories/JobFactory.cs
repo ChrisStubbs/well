d8b291c39d5bc370620ba90ca3a9729ce0e5d3d2
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-05-12 11:14:50 +0100

    Test on mappers

--- src/4. Test/UnitTests/Factories/JobFactory.cs
+++ src/4. Test/UnitTests/Factories/JobFactory.cs
@@ -9,34 +9,48 @@
     {
         public JobFactory()
         {
-            this.Entity.Id = 1;
-            this.Entity.Sequence = 1;
-            this.Entity.JobTypeCode = "001";
-            this.Entity.PhAccount = "J001";
-            this.Entity.PickListRef = "J002";
-            this.Entity.InvoiceNumber = "J0032";
-            this.Entity.CustomerRef = "J004";
-            this.Entity.OrderDate = DateTime.Now;
-            this.Entity.RoyaltyCode = "Royal1";
-            this.Entity.RoyaltyCodeDesc = "RoyalDesc";
-            this.Entity.PerformanceStatus = PerformanceStatus.Narri;
-            this.Entity.JobByPassReason = "some reason";
-            this.Entity.StopId = 1;
+            Entity.Id = 1;
+            Entity.Sequence = 1;
+            Entity.JobTypeCode = "001";
+            Entity.PhAccount = "J001";
+            Entity.PickListRef = "J002";
+            Entity.InvoiceNumber = "J0032";
+            Entity.CustomerRef = "J004";
+            Entity.OrderDate = DateTime.Now;
+            Entity.RoyaltyCode = "Royal1";
+            Entity.RoyaltyCodeDesc = "RoyalDesc";
+            Entity.PerformanceStatus = PerformanceStatus.Narri;
+            Entity.JobByPassReason = "some reason";
+            Entity.StopId = 1;
 
-            this.Entity.EntityAttributes = new List<EntityAttribute>();
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "GrnNumber", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "GrnRefusedReason", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "ActionLogNumber", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "IsOverage", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "OuterCount", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "OuterDiscrepancyFound", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "TotalOutersOver", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "TotalOutersShort", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "Picked", Value = null });
-            this.Entity.EntityAttributes.Add(new EntityAttribute { Code = "InvoiceValue", Value = null });
+            Entity.EntityAttributes = new List<EntityAttribute>();
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "GrnNumber", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "GrnRefusedReason", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "ActionLogNumber", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "IsOverage", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "OuterCount", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "OuterDiscrepancyFound", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "TotalOutersOver", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "TotalOutersShort", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "Picked", Value = null });
+            Entity.EntityAttributes.Add(new EntityAttribute { Code = "InvoiceValue", Value = null });
 
-            this.Entity.EntityAttributeValues = new List<EntityAttributeValue>();
+            Entity.EntityAttributeValues = new List<EntityAttributeValue>();
+        }
 
+        public JobFactory WithTotalShort(int? value)
+        {
+            var totshort = new EntityAttribute { Code = "TOTSHORT" };
+            var totalShortEntityAttributeValue = new EntityAttributeValue {EntityAttribute = totshort, Value = value.ToString()};
+            Entity.EntityAttributeValues.Add(totalShortEntityAttributeValue);
+            return this;
+        }
+
+        public JobFactory WithCod(string value)
+        {
+            var codEntityAttribute = new EntityAttribute { Code = "COD", Value = value };
+            Entity.EntityAttributes.Add(codEntityAttribute);
+            return this;
         }
     }
 }

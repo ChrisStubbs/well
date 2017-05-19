92c60527ffbcbfbfc651a1928a24dd7753aa1f8a
fiona.pond <fiona.pond@HO-NT-VMFPW2K81>
2017-05-19 10:43:45 +0100

    Status for uplift on import

--- src/1. Layers/1.3 Domain/Well.Domain/ValueObjects/EditLineItemException.cs
+++ src/1. Layers/1.3 Domain/Well.Domain/ValueObjects/EditLineItemException.cs
@@ -11,12 +11,6 @@
         public int? Invoiced { get; set; }
         public int? Delivered { get; set; }
         public int Quantity { get; set; }
-        public IList<EditLIneItemExceptionDetail> Exceptions { get; set; }
-    }
-
-    public class EditLIneItemExceptionDetail
-    {
-        public int Quantity { get; set; }
         public string Originator { get; set; }
         public string Exception { get; set; }
         public string Action { get; set; }
@@ -26,5 +20,20 @@
         public string ActionedBy { get; set; }
         public string ApprovedBy { get; set; }
         public IList<string> Comments { get; set; }
+        // public IList<EditLIneItemExceptionDetail> Exceptions { get; set; }
     }
+
+    //public class EditLIneItemExceptionDetail
+    //{
+    //    public int Quantity { get; set; }
+    //    public string Originator { get; set; }
+    //    public string Exception { get; set; }
+    //    public string Action { get; set; }
+    //    public string Source { get; set; }
+    //    public string Reason { get; set; }
+    //    public DateTime? Erdd { get; set; }
+    //    public string ActionedBy { get; set; }
+    //    public string ApprovedBy { get; set; }
+    //    public IList<string> Comments { get; set; }
+    //}
 }

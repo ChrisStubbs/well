0f1127e92f4384111678fae5b045a58f0169b635
fiona.pond <fiona.pond@HO-NT-VMFPW2K81>
2017-05-24 10:40:58 +0100

    Refactor EditLineItemException to include list of exceptions

--- src/1. Layers/1.1 Presentation/Well.Api/Mapper/LineItemExceptionMapper.cs
+++ src/1. Layers/1.1 Presentation/Well.Api/Mapper/LineItemExceptionMapper.cs
@@ -9,51 +9,41 @@
     public class LineItemExceptionMapper : ILineItemExceptionMapper
     {
         public IEnumerable<EditLineItemException> Map(IEnumerable<LineItem> lineItems)
-            
+
         {
             var result = new List<EditLineItemException>();
 
             foreach (var line in lineItems)
             {
-                if (line.LineItemActions.Count == 0)
+                var editLineItemException = new EditLineItemException
                 {
-                    var editLineItemException = new EditLineItemException
+                    Id = line.Id,
+                    ProductNumber = line.ProductCode,
+                    Product = line.ProductDescription,
+                    Invoiced = line.OriginalDespatchQuantity,
+                    Delivered = line.DeliveredQuantity,
+                };
+
+                foreach (var action in line.LineItemActions)
+                {
+                    var editLineItemExceptionDetail = new EditLineItemExceptionDetail
                     {
-                        Id = line.Id,
-                        ProductNumber = line.ProductCode,
-                        Product = line.ProductDescription,
-                        Invoiced = line.OriginalDespatchQuantity,
-                        Delivered = line.DeliveredQuantity,
+                        Originator = EnumExtensions.GetDescription(action.Originator),
+                        Exception = EnumExtensions.GetDescription(action.ExceptionType),
+                        Quantity = action.Quantity,
+                        Source = EnumExtensions.GetDescription(action.Source),
+                        Reason = EnumExtensions.GetDescription(action.Reason),
+                        Erdd = action.ReplanDate,
+                        ActionedBy = action.ActionedBy,
+                        ApprovedBy = action.ApprovedBy
+
                     };
 
-                    result.Add(editLineItemException);
+                    editLineItemException.Exceptions.Add(editLineItemExceptionDetail);
 
                 }
-                else
-                {
-                    foreach (var action in line.LineItemActions)
-                    {
-                        var editLineItemException = new EditLineItemException
-                        {
-                            Id = line.Id,
-                            LineItemActionId = action.Id,
-                            ProductNumber = line.ProductCode,
-                            Product = line.ProductDescription,
-                            Originator = EnumExtensions.GetDescription(action.Originator), 
-                            Exception = EnumExtensions.GetDescription(action.ExceptionType),
-                            Invoiced = line.OriginalDespatchQuantity,
-                            Delivered = line.DeliveredQuantity,
-                            Quantity = action.Quantity,
-                            Source = EnumExtensions.GetDescription(action.Source),
-                            Reason = EnumExtensions.GetDescription(action.Reason),
-                            Erdd = action.ReplanDate,
-                            ActionedBy = action.ActionedBy,
-                            ApprovedBy = action.ApprovedBy
 
-                        };
-                        result.Add(editLineItemException);
-                    }
-                }
+                result.Add(editLineItemException);
             }
 
             return result;
--- src/1. Layers/1.3 Domain/Well.Domain/ValueObjects/EditLineItemException.cs
+++ src/1. Layers/1.3 Domain/Well.Domain/ValueObjects/EditLineItemException.cs
@@ -3,42 +3,22 @@
     using System;
     using System.Collections.Generic;
 
-    //public class EditLineItemException
-    //{
-    //    public int Id { get; set; }
-    //    public string ProductNumber { get; set; }
-    //    public string Product { get; set; }
-    //    public int? Invoiced { get; set; }
-    //    public int? Delivered { get; set; }
-    //    public int Quantity { get; set; }
-    //    public IList<EditLIneItemExceptionDetail> Exceptions { get; set; }
-    //}
-
-    //public class EditLIneItemExceptionDetail
-    //{
-    //    public int Quantity { get; set; }
-    //    public string Originator { get; set; }
-    //    public string Exception { get; set; }
-    //    public string Action { get; set; }
-    //    public string Source { get; set; }
-    //    public string Reason { get; set; }
-    //    public DateTime? Erdd { get; set; }
-    //    public string ActionedBy { get; set; }
-    //    public string ApprovedBy { get; set; }
-    //    public IList<string> Comments { get; set; }
-    //}
-
     public class EditLineItemException
     {
         public int Id { get; set; }
-        public int? LineItemActionId { get; set; }
         public string ProductNumber { get; set; }
         public string Product { get; set; }
-        public string Originator { get; set; }
-        public string Exception { get; set; }
         public int? Invoiced { get; set; }
         public int? Delivered { get; set; }
         public int Quantity { get; set; }
+        public IList<EditLineItemExceptionDetail> Exceptions { get; set; }
+    }
+
+    public class EditLineItemExceptionDetail
+    {
+        public int Quantity { get; set; }
+        public string Originator { get; set; }
+        public string Exception { get; set; }
         public string Action { get; set; }
         public string Source { get; set; }
         public string Reason { get; set; }
@@ -46,14 +26,19 @@
         public string ActionedBy { get; set; }
         public string ApprovedBy { get; set; }
         public IList<string> Comments { get; set; }
-        // public IList<EditLIneItemExceptionDetail> Exceptions { get; set; }
     }
 
-    //public class EditLIneItemExceptionDetail
+    //public class EditLineItemException
     //{
-    //    public int Quantity { get; set; }
+    //    public int Id { get; set; }
+    //    public int? LineItemActionId { get; set; }
+    //    public string ProductNumber { get; set; }
+    //    public string Product { get; set; }
     //    public string Originator { get; set; }
     //    public string Exception { get; set; }
+    //    public int? Invoiced { get; set; }
+    //    public int? Delivered { get; set; }
+    //    public int Quantity { get; set; }
     //    public string Action { get; set; }
     //    public string Source { get; set; }
     //    public string Reason { get; set; }
@@ -61,5 +46,7 @@
     //    public string ActionedBy { get; set; }
     //    public string ApprovedBy { get; set; }
     //    public IList<string> Comments { get; set; }
+    //    // public IList<EditLIneItemExceptionDetail> Exceptions { get; set; }
     //}
+
 }

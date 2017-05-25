db032072ac466b7516fa0d4c67d4e2fd918b2e57
fiona.pond <fiona.pond@HO-NT-VMFPW2K81>
2017-05-25 09:16:46 +0100

    Views for Exception counts on LineItem, Activity, Location

---
+++ src/3. Database/Well/dbo/Views/LineItemExceptionsView.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW LineItemExceptionsView AS
+
+WITH Exception (LineItemId, ExceptionTypeId, ExceptionCount) AS
+( SELECT li.id, ExceptionTypeId, COUNT(ExceptionTypeId)
+FROM LineItem li
+INNER JOIN LineItemAction lia ON li.Id = lia.LineItemId
+GROUP BY li.Id, ExceptionTypeId
+)
+
+SELECT li.Id
+	, COUNT(lia.Id) AS TotalExceptions
+	, ISNULL((SELECT ExceptionCount FROM Exception WHERE LineItemId = li.Id AND ExceptionTypeId = 1) ,0) AS ShortTotal
+	, ISNULL((SELECT ExceptionCount FROM Exception WHERE LineItemId = li.Id AND ExceptionTypeId = 2) ,0) AS BypassTotal
+	, ISNULL((SELECT ExceptionCount FROM Exception WHERE LineItemId = li.Id AND ExceptionTypeId = 3) ,0) AS DamageTotal
+FROM LineItem li
+INNER JOIN LineItemAction lia ON lia.LineItemId = li.Id
+GROUP BY li.Id

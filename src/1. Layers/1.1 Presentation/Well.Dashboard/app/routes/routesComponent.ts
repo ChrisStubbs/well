8012552f824b41a6ccd317e99465198e5a312250
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-05-26 09:05:44 +0100

    Assign route to user

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/routes/routesComponent.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/routes/routesComponent.ts
@@ -1,20 +1,22 @@
-﻿import { ActivatedRoute }                           from '@angular/router';
-import { Component, ViewChild }                     from '@angular/core';
-import { GlobalSettingsService }                    from '../shared/globalSettings';
-import { Route }                                    from './route';
-import { RouteFilter }                              from './routeFilter';
-import { RoutesService }                            from './routesService';
-import { RefreshService }                           from '../shared/refreshService';
-import { SecurityService }                          from '../shared/security/securityService';
-import { BranchService }                            from '../shared/branch/branchService';
-import { AppSearchParameters }                      from '../shared/appSearch/appSearch';
-import { DataTable }                                from 'primeng/primeng';
-import { AssignModel }                              from '../shared/assignModel';
-import { Branch }                                   from '../shared/branch/branch';
-import { AppDefaults }                              from '../shared/defaults/defaults';
-import { IObservableAlive }                         from '../shared/IObservableAlive';
-import { LookupService, LookupsEnum, ILookupValue}  from '../shared/services/services';
+﻿import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { GlobalSettingsService } from '../shared/globalSettings';
+import { Route } from './route';
+import { RouteFilter } from './routeFilter';
+import { RoutesService } from './routesService';
+import { RefreshService } from '../shared/refreshService';
+import { SecurityService } from '../shared/security/securityService';
+import { BranchService } from '../shared/branch/branchService';
+import { AppSearchParameters } from '../shared/appSearch/appSearch';
+import { DataTable } from 'primeng/primeng';
+import { AssignModel } from '../shared/assignModel';
+import { Branch } from '../shared/branch/branch';
+import { AppDefaults } from '../shared/defaults/defaults';
+import { IObservableAlive } from '../shared/IObservableAlive';
+import { LookupService, LookupsEnum, ILookupValue } from '../shared/services/services';
 import 'rxjs/Rx';
+import { AssignModalResult } from '../shared/assignModel';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'ow-route',
@@ -50,7 +52,7 @@ export class RoutesComponent implements IObservableAlive
         private refreshService: RefreshService,
         private activatedRoute: ActivatedRoute,
         protected securityService: SecurityService,
-        private branchService: BranchService) {}
+        private branchService: BranchService) { }
 
     public ngOnInit()
     {
@@ -115,6 +117,14 @@ export class RoutesComponent implements IObservableAlive
 
     public onAssigned($event)
     {
-        this.getRoutes();
+        const result = $event as AssignModalResult;
+        if (result.assigned)
+        {
+            const route = _.find(this.routes, (x) => x.id === result.source.id) as Route;
+            if (route)
+            {
+                route.assignee = (result.newUser) ? result.newUser.name : 'Unallocated';
+            }
+        }
     }
 }

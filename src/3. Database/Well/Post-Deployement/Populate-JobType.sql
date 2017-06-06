1cc813ebee0818f267ec97cb409995e31c131855
Henrry.Pires <henrry.pires@palmerharvey.co.uk>
2017-06-06 13:30:30 +0100

    fix the build

--- src/3. Database/Well/Post-Deployement/Populate-JobType.sql
+++ src/3. Database/Well/Post-Deployement/Populate-JobType.sql
@@ -1,32 +1,32 @@
-﻿SET IDENTITY_INSERT [JobType] ON
+﻿SET IDENTITY_INSERT JobType ON
 
-MERGE INTO [JobType] AS Target
-USING	(VALUES	(1,'DEL-TOB','Tobacco', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(2,'DEL-AMB','Ambient', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(3,'DEL-ALC','Alchohol', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(4,'DEL-CHL','Chilled', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(5,'DEL-FRZ','Frozen', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(6,'DEL-DOC','Documents', dbo.ActivityType_Documents(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(7,'UPL-SAN','Uplift – Sandwiches', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(8,'UPL-GLO','Uplift – Global', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(9,'UPL-ASS','Uplift – Scheduled Asset Uplift', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(10,'UPL-STD','Uplift – Standard', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
-				(11,'NotDef','Not Defined', dbo.ActivityType_NotDefined(),'deployment',GETDATE(),'deployment',GETDATE())
+MERGE INTO JobType AS Target
+USING	(VALUES	(1,'DEL-TOB','Tobacco', 'Tob', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(2,'DEL-AMB','Ambient', 'Amb', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(3,'DEL-ALC','Alchohol', 'Alc', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(4,'DEL-CHL','Chilled', 'Chi', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(5,'DEL-FRZ','Frozen', 'Frz', dbo.ActivityType_Invoice(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(6,'DEL-DOC','Documents', 'Doc', dbo.ActivityType_Documents(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(7,'UPL-SAN','Uplift – Sandwiches', 'USD', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(8,'UPL-GLO','Uplift – Global', 'UGL', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(9,'UPL-ASS','Uplift – Scheduled Asset Uplift', 'USA', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(10,'UPL-STD','Uplift – Standard', 'UST', dbo.ActivityType_Uplift(),'deployment',GETDATE(),'deployment',GETDATE()),
+				(11,'NotDef','Not Defined', '', dbo.ActivityType_NotDefined(),'deployment',GETDATE(),'deployment',GETDATE())
 		)
-AS Source ([Id],[Code],[Description],[ActivityTypeId],[CreatedBy],[CreatedDate],[LastUpdatedBy],[LastUpdatedDate])
-	ON Target.[Id] = Source.[Id]
+AS Source (Id,Code,Description,Abbreviation, ActivityTypeId,CreatedBy,CreatedDate,LastUpdatedBy,LastUpdatedDate)
+	ON Target.Id = Source.Id
 WHEN MATCHED 
-	AND (Target.[Description] != Source.[Description])
+	AND (Target.Description != Source.Description)
 	THEN
 	UPDATE SET 
-			[Code]				= Source.[Code],
-			[Description]		= Source.[Description], 
-			[ActivityTypeId]    = Source.[ActivityTypeId],
-			[LastUpdatedBy]		= Source.[LastUpdatedBy], 
-			[LastUpdatedDate]	= Source.[LastUpdatedDate]	
+			Code			= Source.Code,
+			Description		= Source.Description, 
+			ActivityTypeId  = Source.ActivityTypeId,
+			LastUpdatedBy	= Source.LastUpdatedBy, 
+			LastUpdatedDate	= Source.LastUpdatedDate	
 WHEN NOT MATCHED BY TARGET THEN
-	INSERT ([Id],[Code],[Description],[ActivityTypeId],[CreatedBy],[CreatedDate],[LastUpdatedBy],[LastUpdatedDate])
-	VALUES ([Id],[Code],[Description],[ActivityTypeId],[CreatedBy],[CreatedDate],[LastUpdatedBy],[LastUpdatedDate]);
+	INSERT (Id,Code,Description,ActivityTypeId,CreatedBy,CreatedDate,LastUpdatedBy,LastUpdatedDate)
+	VALUES (Id,Code,Description,ActivityTypeId,CreatedBy,CreatedDate,LastUpdatedBy,LastUpdatedDate);
 
 UPDATE JobType
 SET Abbreviation = Data.Abbreviation 
@@ -47,4 +47,4 @@ FROM
 WHERE
 	Data.Id = JobType.Id
 
-SET IDENTITY_INSERT [JobType] OFF
+SET IDENTITY_INSERT JobType OFF

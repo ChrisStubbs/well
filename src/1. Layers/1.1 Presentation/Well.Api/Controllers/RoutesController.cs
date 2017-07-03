7fb55c52fd2d176df13db1535eaf4455d9195b60
Henrry.Pires <henrry.pires@palmerharvey.co.uk>
2017-07-03 12:28:03 +0100

    EOW-885 Planned routes showing as having exceptions

--- src/1. Layers/1.1 Presentation/Well.Api/Controllers/RoutesController.cs
+++ src/1. Layers/1.1 Presentation/Well.Api/Controllers/RoutesController.cs
@@ -10,31 +10,18 @@
     public class RoutesController : BaseApiController
     {
         private readonly IRouteReadRepository routeReadRepository;
-        private readonly IServerErrorResponseHandler serverErrorResponseHandler;
 
         public RoutesController(IRouteReadRepository routeReadRepository,
-                IServerErrorResponseHandler serverErrorResponseHandler,
                 IUserNameProvider userNameProvider) :
             base(userNameProvider)
         {
             this.routeReadRepository = routeReadRepository;
-            this.serverErrorResponseHandler = serverErrorResponseHandler;
-
         }
 
         public HttpResponseMessage Get(int branchId)
         {
-            try
-            {
-                return this.Request.CreateResponse(HttpStatusCode.OK,
-                    this.routeReadRepository.GetAllRoutesForBranch(branchId, this.UserIdentityName).ToList());
-            }
-            catch (Exception ex)
-            {
-
-                return serverErrorResponseHandler.HandleException(Request, ex, "An error occurred when getting routes");
-            }
+            return this.Request.CreateResponse(HttpStatusCode.OK,
+                this.routeReadRepository.GetAllRoutesForBranch(branchId, this.UserIdentityName).ToList());
         }
-
     }
 }

8ae3c2510822511137b7d53ee608b8d84f8f7919
fiona.pond <fiona.pond@palmerharvey.co.uk>
2017-07-17 10:43:15 +0100

    EOW-961 Skip update for Job that has ResolutionStatus of ManuallyCompleted; log & raise event for SCOM.

--- src/1. Layers/1.2 Services/Well.Services/EpodServices/EpodUpdateService.cs
+++ src/1. Layers/1.2 Services/Well.Services/EpodServices/EpodUpdateService.cs
@@ -221,6 +221,19 @@
                     continue;
                 }
 
+                if (existingJob.ResolutionStatus == ResolutionStatus.ManuallyCompleted)
+                {
+                    this.logger.LogError(
+                        $"Job update ignored because it has been manually completed ({existingJob.Id}), StopId ({existingJob.StopId})");
+                      
+                    this.eventLogger.TryWriteToEventLog(
+                        EventSource.WellAdamXmlImport,
+                        $"Job update ignored because it has been manually completed ({existingJob.Id}), StopId ({existingJob.StopId})",
+                        9860);
+
+                    continue;
+                }
+
                 existingJob.ResolutionStatus = ResolutionStatus.DriverCompleted;
                 updatedJobs.Add(UpdateJob(jobDto, existingJob, branchId, routeDate));
 
@@ -234,8 +247,6 @@
             this.routeMapper.Map(jobDto, existingJob);
 
             this.jobService.DetermineStatus(existingJob, branchId);
-
-
 
             if (!string.IsNullOrWhiteSpace(jobDto.GrnNumber) && existingJob.GrnProcessType == ProcessTypeForGrn)
             {

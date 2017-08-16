434101cd9bf9465de5127f084e48b4065cbc875f
krystian.dudzik <krystian.dudzik@palmerharvey.co.uk>
2017-08-16 17:01:40 +0100

    Updated ExceptionEvent table

--- src/3. Database/Well/dbo/Tables/ExceptionEvent.sql
+++ src/3. Database/Well/dbo/Tables/ExceptionEvent.sql
@@ -5,6 +5,7 @@
 	[ExceptionActionId] INT NOT NULL,
 	[Processed] BIT NOT NULL DEFAULT 0,
 	[DateCanBeProcessed] DATETIME NOT NULL,
+	[SourceId] VARCHAR(50) NULL,
 	[CreatedBy] VARCHAR(50) NOT NULL,
 	[DateCreated] DATETIME NOT NULL,
 	[UpdatedBy] VARCHAR(50) NOT NULL,
@@ -13,3 +14,5 @@
 	CONSTRAINT [PK_ExceptionEvent] PRIMARY KEY CLUSTERED ([Id] ASC),
 	CONSTRAINT [FK_ExceptionAction] FOREIGN KEY ([ExceptionActionId]) REFERENCES [dbo].[ExceptionAction] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
+GO
+CREATE NONCLUSTERED INDEX [IDX_ExceptionEvent_SourceId] ON [dbo].[ExceptionEvent] ([SourceId])

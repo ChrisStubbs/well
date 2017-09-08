38d98189805f199ef767ccb4d665f7967be868a6
Henrry.Pires <pireshenrry@gmail.com>
2017-09-08 13:43:16 +0100

    this came up when i update the packages

---
+++ src/4. Test/UnitTests/T4Example/T4ConstantsExample.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+// =======================================================================
+// GENERATED CONTENT - DO NOT MODIFY THIS FILE. YOUR CHANGES WILL BE LOST!
+// =======================================================================
+namespace PH.Shared.ProjectNameHere.Constants
+{
+	/// <summary>
+	/// Constants for the ???? Type names
+	/// </summary>
+	public class ProductTypeNames
+	{
+			/// <summary>
+			/// Alcohol
+			/// </summary>
+			public const string Alcohol = "Alcohol";				
+
+			/// <summary>
+			/// Alcohol Singles
+			/// </summary>
+			public const string AlcoholSingles = "Alcohol Singles";		
+
+			/// <summary>
+			/// Chilled
+			/// </summary>
+			public const string Chilled = "Chilled";				
+
+			/// <summary>
+			/// Cigarettes
+			/// </summary>
+			public const string Cigarettes = "Cigarettes";			
+
+			/// <summary>
+			/// Cigars
+			/// </summary>
+			public const string Cigars = "Cigars";				
+
+			/// <summary>
+			/// Confectionery-Choc
+			/// </summary>
+			public const string ConfectioneryChoc = "Confectionery-Choc";	
+
+			/// <summary>
+			/// Confectionery-Sugar
+			/// </summary>
+			public const string ConfectionerySugar = "Confectionery-Sugar";	
+
+			/// <summary>
+			/// Crisps
+			/// </summary>
+			public const string Crisps = "Crisps";				
+
+			/// <summary>
+			/// Drinks
+			/// </summary>
+			public const string Drinks = "Drinks";				
+
+			/// <summary>
+			/// Frozen
+			/// </summary>
+			public const string Frozen = "Frozen";				
+
+			/// <summary>
+			/// Grocery
+			/// </summary>
+			public const string Grocery = "Grocery";				
+
+			/// <summary>
+			/// Grocery Singles
+			/// </summary>
+			public const string GrocerySingles = "Grocery Singles";		
+
+			/// <summary>
+			/// Household Sundries
+			/// </summary>
+			public const string HouseholdSundries = "Household Sundries";	
+
+			/// <summary>
+			/// Ice cream
+			/// </summary>
+			public const string IceCream = "Ice cream";				
+
+			/// <summary>
+			/// Low Margin Cigarettes
+			/// </summary>
+			public const string LowMarginCigarettes = "Low Margin Cigarettes";	
+
+			/// <summary>
+			/// Sundries Singles
+			/// </summary>
+			public const string SundriesSingles = "Sundries Singles";		
+
+			/// <summary>
+			/// Tobacco
+			/// </summary>
+			public const string Tobacco = "Tobacco";				
+
+			/// <summary>
+			/// Tobacco Sundries
+			/// </summary>
+			public const string TobaccoSundries = "Tobacco Sundries";		
+
+	}
+}
---
+++ src/4. Test/UnitTests/T4Example/T4EnumExample.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.ComponentModel;
+// =======================================================================
+// GENERATED CONTENT - DO NOT MODIFY THIS FILE. YOUR CHANGES WILL BE LOST!
+// =======================================================================
+namespace PH.Shared.ProjectNameHere.Enums
+{
+	/// <summary>
+	/// Enums for the various ???? Types
+	/// </summary>
+	public enum eProductType
+	{
+			[Description("Cigarettes")]
+			Cigarettes				= 1,	
+
+			[Description("Low Margin Cigarettes")]
+			LowMarginCigarettes		= 2,	
+
+			[Description("Tobacco")]
+			Tobacco					= 3,	
+
+			[Description("Cigars")]
+			Cigars					= 4,	
+
+			[Description("Tobacco Sundries")]
+			TobaccoSundries			= 5,	
+
+			[Description("Confectionery-Choc")]
+			ConfectioneryChoc		= 6,	
+
+			[Description("Confectionery-Sugar")]
+			ConfectionerySugar		= 7,	
+
+			[Description("Crisps")]
+			Crisps					= 8,	
+
+			[Description("Drinks")]
+			Drinks					= 9,	
+
+			[Description("Household Sundries")]
+			HouseholdSundries		= 10,	
+
+			[Description("Grocery")]
+			Grocery					= 12,	
+
+			[Description("Sundries Singles")]
+			SundriesSingles			= 13,	
+
+			[Description("Grocery Singles")]
+			GrocerySingles			= 14,	
+
+			[Description("Chilled")]
+			Chilled					= 15,	
+
+			[Description("Frozen")]
+			Frozen					= 16,	
+
+			[Description("Alcohol")]
+			Alcohol					= 17,	
+
+			[Description("Ice cream")]
+			IceCream				= 18,	
+
+			[Description("Alcohol Singles")]
+			AlcoholSingles			= 19	
+
+	}
+}

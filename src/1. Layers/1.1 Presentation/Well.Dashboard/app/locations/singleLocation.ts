cd01eacfe9500fb338388d87850d7a03ac480d6a
Henrry.Pires <pireshenrry@gmail.com>
2017-09-20 11:50:39 +0100

    EOW-1485 Exceptions / Clean filter on Single location/route pages

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/locations/singleLocation.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/locations/singleLocation.ts
@@ -116,7 +116,7 @@ export class SingleLocationFilter implements IFilter
                         return sourceRow.exceptions > 0;
                     }
 
-                    return sourceRow.exceptions == 0;
+                    return sourceRow.clean > 0;
                 };
 
             case 'resolutionId':

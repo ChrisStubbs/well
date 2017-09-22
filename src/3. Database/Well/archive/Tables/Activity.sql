f70ae32e55161232c781395caf6edd2f3c90f22e
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-09-22 11:07:29 +0100

    Tables
    Signed-off-by: Henrry.Pires <pireshenrry@gmail.com>

---
+++ src/3. Database/Well/archive/Tables/Activity.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [archive].[Activity]
+(
+	[Id] INT NOT NULL,
+	[DocumentNumber] VARCHAR(40) NULL,
+	[InitialDocument] VARCHAR(40) NOT NULL,
+	[ActivityTypeId] TINYINT NOT NULL,
+	[LocationId] INT NULL,
+	[CreatedBy] VARCHAR(50) NOT NULL,
+	[CreatedDate] DATETIME NOT NULL,
+	[LastUpdatedBy] VARCHAR(50) NOT NULL,
+	[LastUpdatedDate] DATETIME NOT NULL,
+	[Version] [TIMESTAMP] NOT NULL,
+    [DateDeleted] DATETIME NULL, 
+ ) ON ARCHIVE
---
+++ src/3. Database/Well/archive/Tables/LineItemActionComment.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [archive].[LineItemActionComment]
+(
+	[Id] INT NOT NULL,
+	[LineItemActionId] INT NOT NULL,
+	[CommentReasonId] INT NOT NULL,
+	[FromQty] INT NULL DEFAULT 0,
+	[ToQty] INT NOT NULL DEFAULT 0,
+	[CreatedBy] VARCHAR(50) NOT NULL,
+	[DateCreated] DATETIME NOT NULL,
+	[UpdatedBy] VARCHAR(50) NOT NULL,
+	[DateUpdated] DATETIME NOT NULL,
+	[DateDeleted] DATETIME NULL,
+	[DeletedByImport] BIT DEFAULT 0,
+	[Version] [TIMESTAMP] NOT NULL,
+	[ArchiveDate] SMALLDATETIME NOT NULL
+)ON Archive

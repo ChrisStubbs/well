379440b732d5273400b11dc459e17fa4d77bfd3c
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-09-28 11:43:40 +0100

    EOW-1124 It is currently not possible to manually complete a bypassed job

--- src/3. Database/Well/dbo/StoredProcedures/LineItemAction_DeleteForJob.sql
+++ src/3. Database/Well/dbo/StoredProcedures/LineItemAction_DeleteForJob.sql
@@ -1,20 +1,25 @@
 ﻿CREATE PROCEDURE [dbo].[LineItemAction_DeleteForJob]
-	@JobId INT,
-	@UpdatedBy VARCHAR(50),
-	@UpdatedDate DATETIME
+	@JobId INT
 AS
 BEGIN
 	SET NOCOUNT ON
 
-	UPDATE lia
-	SET	
-		LastUpdatedBy = @UpdatedBy,
-		LastUpdatedDate = @UpdatedDate,
-		DateDeleted = @UpdatedDate
+	DELETE liac
+	FROM
+		LineItemActionComment liac
+	INNER JOIN
+		LineItemAction lia On lia.id =  liac.LineItemActionId
+	INNER JOIN 
+		JobDetail jd on lia.LineItemId = jd.LineItemId
+	WHERE jd.JobId = @JobId
+
+	DELETE lia
 	FROM 
 		LineItemAction lia
-	INNER JOIN JobDetail jd on lia.LineItemId = jd.LineItemId
-	WHERE jd.JobId = @JobId
+	INNER JOIN 
+		JobDetail jd on lia.LineItemId = jd.LineItemId
+	WHERE 
+		jd.JobId = @JobId
 
 END
 		

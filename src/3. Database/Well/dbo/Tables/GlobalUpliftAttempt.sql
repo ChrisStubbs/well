cdae239d844a35371696a1d35dc6fa5a01fb244f
David.Johnston <David.Johnston@palmerharvey.co.uk>
2017-10-09 10:43:06 +0100

    Added GlobalUplift and GlobalUpliftAttempt tables to Well

---
+++ src/3. Database/Well/dbo/Tables/GlobalUpliftAttempt.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[GlobalUpliftAttempt](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[GlobalUpliftId] [int] NOT NULL,
+	[DateAttempted] [smalldatetime] NOT NULL,
+	[DriverName] [varchar](50) NOT NULL,
+	[RouteNumber] [varchar](12) NOT NULL,
+	[PlannedStopNumber] [varchar](4) NOT NULL,
+	[JobStatusId] [tinyint] NOT NULL,
+	[CollectedQty] [smallint] NULL,
+    CONSTRAINT [PK_GlobalUpliftAttempt] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY], 
+    CONSTRAINT [FK_GlobalUpliftAttempt_ToGlobalUplift] FOREIGN KEY ([GlobalUpliftId]) REFERENCES [GlobalUplift]([Id]), 
+    CONSTRAINT [FK_GlobalUpliftAttempt_ToJobStatus] FOREIGN KEY ([JobStatusId]) REFERENCES [JobStatus]([Id])
+) ON [PRIMARY]
+
+GO
+
+CREATE INDEX [IX_GlobalUpliftAttempt_GlobalUpliftIdDate] ON [dbo].[GlobalUpliftAttempt] ([GlobalUpliftId], [DateAttempted])

519b495e19a918386f18bd85f2802a124d70f555
Henrry.Pires <pireshenrry@gmail.com>
2017-10-18 15:14:37 +0100

    EOW-1721 Clean process needs seasonal dates changed

---
+++ src/3. Database/Well/ManualStep/scripts/up/033_fix_seasonal_dates.sql
@@ -0,0 +1,10 @@
+﻿IF (@@DATEFIRST = 7)
+BEGIN
+    -- this script should only be run when the first day of the week is Sunday
+    -- the data was generated under that condition
+    UPDATE SeasonalDate
+        SET [From] = DATEADD(Day, 1, [From]),
+        [To] = DATEADD(Day, 1, [To])
+    WHERE
+        DATEPART(dw, [To]) = 7  AND [Description] Like 'Week - %'
+END

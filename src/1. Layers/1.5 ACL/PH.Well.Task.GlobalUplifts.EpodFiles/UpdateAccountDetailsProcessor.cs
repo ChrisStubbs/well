0b11d275b599e3236ef2eba5ca88176b599c150e
David.Johnston <David.Johnston@palmerharvey.co.uk>
2017-11-15 15:34:36 +0000

    VAD-19: Account details now fetched immediately on Global Uplifts

--- src/1. Layers/1.5 ACL/PH.Well.Task.GlobalUplifts.EpodFiles/UpdateAccountDetailsProcessor.cs
+++ src/1. Layers/1.5 ACL/PH.Well.Task.GlobalUplifts.EpodFiles/UpdateAccountDetailsProcessor.cs
@@ -26,33 +26,32 @@ namespace PH.Well.Task.GlobalUplifts.EpodFiles
         #region Public methods
         public void Run()
         {
-            var globalUpliftAttempts = _wellEntities.GlobalUpliftAttempt.Include("GlobalUplift")
-                .Where(x => x.DateBranchEmailSent == null && x.CollectedQty > 0 && !x.GlobalUplift.HasAccountInfo)
+            var globalUplifts = _wellEntities.GlobalUplift
+                .Where(x => !x.HasAccountInfo)
                 .ToList();
-            foreach (var globalUpliftAttempt in globalUpliftAttempts)
+            foreach (var globalUplift in globalUplifts)
             {
                 // Use the Account service to fetch account details for these global uplifts
-                var account = accountService.GetAccount(globalUpliftAttempt.GlobalUplift.BranchId,
-                    globalUpliftAttempt.GlobalUplift.PHAccount);
+                var account = accountService.GetAccount(globalUplift.BranchId,
+                    globalUplift.PHAccount);
 
                 if (account != null)
                 {
-                    globalUpliftAttempt.GlobalUplift.AccountName = account.AccountName;
-                    globalUpliftAttempt.GlobalUplift.AddressLines = account.DeliveryAddress?.AddressLines;
-                    globalUpliftAttempt.GlobalUplift.Postcode = account.DeliveryAddress?.Postcode;
+                    globalUplift.AccountName = account.AccountName;
+                    globalUplift.AddressLines = account.DeliveryAddress?.AddressLines;
+                    globalUplift.Postcode = account.DeliveryAddress?.Postcode;
 
                     var primaryContact = account.Contacts.FirstOrDefault(x => x.IsPrimaryContact);
                     if (primaryContact != null)
                     {
-                        globalUpliftAttempt.GlobalUplift.ContactName = primaryContact.Name;
-                        globalUpliftAttempt.GlobalUplift.ContactNumber = primaryContact.Phone;
+                        globalUplift.ContactName = primaryContact.Name;
+                        globalUplift.ContactNumber = primaryContact.Phone;
                     }
 
-                    if (!string.IsNullOrWhiteSpace(globalUpliftAttempt.GlobalUplift.AccountName) &&
-                        !string.IsNullOrWhiteSpace(globalUpliftAttempt.GlobalUplift.AddressLines) &&
-                        !string.IsNullOrWhiteSpace(globalUpliftAttempt.GlobalUplift.Postcode))
+                    if (!string.IsNullOrWhiteSpace(globalUplift.AccountName) &&
+                        !string.IsNullOrWhiteSpace(globalUplift.AddressLines))
                     {
-                        globalUpliftAttempt.GlobalUplift.HasAccountInfo = true;
+                        globalUplift.HasAccountInfo = true;
                     }
                 }
             }

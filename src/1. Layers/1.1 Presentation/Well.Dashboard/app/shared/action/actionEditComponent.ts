c009a8a05ad6ffc9960bec8e75148fadfd50db57
Henrry.Pires <pireshenrry@gmail.com>
2017-11-24 15:36:11 +0000

    more changes to files to deploy

--- src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/action/actionEditComponent.ts
+++ src/1. Layers/1.1 Presentation/Well.Dashboard/app/shared/action/actionEditComponent.ts
@@ -18,7 +18,6 @@ import { LineItemAction }           from '../../exceptions/lineItemAction';
 import { EditLineItemException }    from '../../exceptions/editLineItemException';
 import { LineItemActionComment }    from '../../exceptions/lineItemAction';
 import { SecurityService }          from '../services/securityService';
-import { UpliftAction }             from '../../job/job';
 
 @Component({
     selector: 'action-edit',
